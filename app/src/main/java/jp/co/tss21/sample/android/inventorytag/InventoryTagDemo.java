b15db4a851f8009989db97abf85f9591ad4145ef
堀江 <22nm663y@vc.ibaraki.ac.jp>
2022-09-26 16:31:56 +0900

    220926 堀江

--- app/src/main/java/jp/co/tss21/sample/android/inventorytag/InventoryTagDemo.java
+++ app/src/main/java/jp/co/tss21/sample/android/inventorytag/InventoryTagDemo.java
@@ -85,7 +85,7 @@ public class InventoryTagDemo extends TabActivity implements View.OnClickListene
     private Button mBtnDevForward;
 
     /** CALタブ用 */
-
+    //220926 ほりえ
     private Button mBtnOptBack;
     private Button mBtnOptForward;
     private Button mBtnMapLab;//学生室ボタン
